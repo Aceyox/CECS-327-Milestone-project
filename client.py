614ab882ad9509e89ca908d137fe2f2630b9ad19
Aryanp44 <145080791+Aryanp44@users.noreply.github.com>
2025-10-21 19:47:02 -0700

    Update client.py

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import uuid
 HOST = "127.0.0.1"
 PORT = 5050
 
+#Dictionary for an example alert message 
 alert = {
     "id": str(uuid.uuid4()),
     "disaster_type": "Earthquake",
@@ -13,11 +14,16 @@ alert = {
     "magnitude": 5.8
 }
 
+#Creates a TCP socket and connect to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+#Convert the dictionary to JSON and send it to the server
 s.sendall(json.dumps(alert).encode())
 
+#Wait for the server's response and decode it
 data = s.recv(1024).decode()
+
 print("")
 print("Server reply:", data)
 print("")
