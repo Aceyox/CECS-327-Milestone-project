fc48122956c8c8afbc3ea96642dff46a43902bb4
Aryanp44 <145080791+Aryanp44@users.noreply.github.com>
2025-11-04 13:25:47 -0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,32 +1,18 @@
 import socket
 import json
-import uuid
 
 HOST = "127.0.0.1"
 PORT = 5050
 
-#Dictionary for an example alert message 
 alert = {
-    "id": str(uuid.uuid4()),
     "disaster_type": "Earthquake",
     "region": "California",
     "severity": "High",
     "magnitude": 5.8
 }
 
-#Creates a TCP socket and connect to the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
-#Convert the dictionary to JSON and send it to the server
-s.sendall(json.dumps(alert).encode())
-
-#Wait for the server's response and decode it
-data = s.recv(1024).decode()
-
-print("")
-print("Server reply:", data)
-print("")
-
-
-s.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(json.dumps(alert).encode())
+    data = s.recv(1024).decode()
+    print("Server reply:", data)
